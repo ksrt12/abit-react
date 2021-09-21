4ec04d0c20ab22c7379af8c8e68c6ed69f63550d
Kazakov Stepan <ksrt12group@gmail.com>
2021-09-21 18:11:56 +0300

    App.js: use 4 spaces
    Signed-off-by: Kazakov Stepan <ksrt12group@gmail.com>

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ import Copyright from "./UI/Copyright";
 import { fromWLS } from "./js/constants";
 
 function App() {
-  return (
-    <div>
-      <Links />
-      <div className="main">
-        <br />
-        {fromWLS ? null : (<SearchForm />)}
-        <div id="results"></div>
-      </div>
-      <Copyright />
-    </div>
-  );
+    return (
+        <div>
+            <Links />
+            <div className="main">
+                <br />
+                {fromWLS ? null : (<SearchForm />)}
+                <div id="results"></div>
+            </div>
+            <Copyright />
+        </div>
+    );
 }
 
 export default App;

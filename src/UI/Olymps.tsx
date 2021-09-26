0a148c97f1533948b810749ee2155b410c7255c3
Stepan Kazakov <ksrt12group@gmail.com>
2021-09-26 05:15:56 +0300

    Olymp: convert to tsx
    Signed-off-by: Stepan Kazakov <ksrt12group@gmail.com>

--- src/UI/Olymps.tsx
+++ src/UI/Olymps.tsx
@@ -2,7 +2,16 @@ import { RSROLYMP } from "../ts/constants";
 import { colorBVI } from "../ts/colors";
 import { checkBVI } from "../ts/bvi";
 
-function getSubTitles(olympName) {
+function MakeLink(props: { year: number; code: number; }) {
+    const code = props.code.toString();
+    return (
+        <a href={`${RSROLYMP}${props.year}/by-code/${code}/white.pdf`}>
+            {code.replace(/([0-9]{3})([0-9]{4})([0-9]{4})/, '$1 $2-$3')}
+        </a>
+    );
+}
+
+function getSubTitles(olympName: string) {
     return {
         name: olympName.substring(olympName.indexOf('. "') + 3, olympName.indexOf('("') - 2).replace(/[«»]+/g, '"').trim(),
         lvl: Number(olympName.substr(olympName.indexOf('уровень') - 2, 1).trim()),
@@ -11,17 +20,11 @@ function getSubTitles(olympName) {
     };
 }
 
-function MakeLink({ code, year }) {
-    return (
-        <a href={`${RSROLYMP}${year}/by-code/${code}/white.pdf`}>
-            {`${code}`.replace(/([0-9]{3})([0-9]{4})([0-9]{4})/, '$1 $2-$3')}
-        </a>
-    );
-}
+function OlympRow(props: { d: any; year: number; }) {
 
-
-function OlympRow({ d, year }) {
-    const olymp = getSubTitles(d.oa, d.form);
+    const d = props.d;
+    //console.dir(d);
+    const olymp = getSubTitles(d.oa);
     const status = checkBVI('01.03.02', { grad: d.form, ...olymp });
     return (
         <tr style={{ backgroundColor: colorBVI(status) }} >
@@ -29,14 +32,14 @@ function OlympRow({ d, year }) {
             <td>{olymp.lvl}</td>
             <td>{olymp.dip}</td>
             <td>{olymp.subj}</td>
-            <td><MakeLink code={d.code} year={year} /></td>
+            <td><MakeLink code={d.code} year={props.year} /></td>
             <td>{d.form}</td>
             <td>{status}</td>
         </tr>
     );
 }
 
-function Olymps(year, codes) {
+function Olymps(year: number, codes: any[]) {
     const trs = [];
     for (const d of codes) {
         if (d.form > 9) {

37ec3a017654db696a16b9ed07ef9e06c036e603
Stepan Kazakov <ksrt12group@gmail.com>
2021-09-28 04:05:10 +0300

    reorganize files: move olymp search to separate file
    rename params to person
    split Person.* to makeName() function
    
    Signed-off-by: Stepan Kazakov <ksrt12group@gmail.com>

--- src/ts/diploma.ts
+++ src/ts/diploma.ts
@@ -8,13 +8,14 @@
 
 import { sha256 } from 'js-sha256';
 import { InsertTable, RemoveTable, updateOutside } from "../UI/FullTable";
-import { WLS, fromWLS, RSROLYMP, subjects } from "./constants";
+import { WLS, fromWLS, subjects } from "./constants";
 import { setPinkColor } from "./colors";
+import { searchOlymps } from "./search";
 
 let nonconf = ' Не подтв.',
     yesconf = ' Подтв.';
 
-let params: { [index: string]: string; }, EGE: { [index: string]: number; } = {};
+let person: { [index: string]: string; }, EGE: { [index: string]: number; } = {};
 
 declare global {
     interface Window {
@@ -36,80 +37,26 @@ function getParamsFrom(pars: string, spl1: string, spl2: string, num = false) {
 }
 
 function loadParams() {
-    params = getParamsFrom(WLS.replace('?', ''), '&', '=');
-    if (params.EGE) {
-        EGE = getParamsFrom(params.EGE, ',', ':', true);
+    person = getParamsFrom(WLS.replace('?', ''), '&', '=');
+    if (person.EGE) {
+        EGE = getParamsFrom(person.EGE, ',', ':', true);
     } else {
         // set default EGE points as 100
         Object.values(subjects).forEach(subj => EGE[subj] = 100);
         yesconf = '';
         nonconf = '';
     }
-    Person.makeName();
-    if (params.BDY) {
-        Person.makeBD();
-    }
+    makeName();
 }
 
-const Person = {
-    makeName() {
-        params.NAME = `${params.LN} ${params.FN} ${params.MN}`.replace(/\s+/g, ' ');
-    },
-    makeBD() {
-        params.BD = `${params.BDY}-${params.BDM}-${params.BDD}`;
-    },
-    getDName() {
-        return ((params.DN) ? params.DN + ' ' : '') + params.NAME;
-    },
-    fullName() {
-        return `${params.NAME} ${params.BD}`;
+function makeName() {
+    person.NAME = `${person.LN} ${person.FN} ${person.MN}`.replace(/\s+/g, ' ');
+    person.Dname = ((person.DN) ? person.DN + ' ' : '') + person.NAME;
+    if (person.BDY) {
+        person.BD = `${person.BDY}-${person.BDM}-${person.BDD}`;
     }
+    person.pid = sha256(`${person.NAME} ${person.BD}`);
 };
-
-function getSubTitles(olympName: string) {
-    return {
-        name: olympName.substring(olympName.indexOf('. "') + 3, olympName.indexOf('("') - 2).replace(/[«»]+/g, '"').trim(),
-        lvl: Number(olympName.substr(olympName.indexOf('уровень') - 2, 1).trim()),
-        dip: Number(olympName.substr(olympName.indexOf('Диплом') + 7, 1).trim()),
-        subj: olympName.substring(olympName.indexOf('("') + 2, olympName.indexOf('")')).toLowerCase().replace('cистемы', 'системы').trim(),
-    };
-}
-
-function getOlymps(year: number, codes: any[]) {
-    const trs = [];
-    for (const d of codes) {
-        if (d.form > 9) {
-            trs.push({
-                year: year,
-                code: d.code,
-                grad: d.form,
-                ...getSubTitles(d.oa),
-            });
-        }
-    }
-    return trs;
-}
-
-function loadDiplomaList(year: number, pid: string) {
-    const s = document.createElement('script');
-    s.async = false;
-    s.src = `${RSROLYMP}${year}/by-person-released/${pid}/codes.js`;
-    document.head.appendChild(s);
-    return new Promise(resolve => {
-        s.onload = () => resolve(getOlymps(year, window.diplomaCodes));
-        s.onerror = () => resolve([]);
-    });
-}
-
-function searchOlymps() {
-    const personID = sha256(Person.fullName());
-    const currYear = new Date().getFullYear();
-    const years = [];
-    for (let year = currYear; year >= currYear - 4; year--) {
-        years.push(loadDiplomaList(year, personID));
-    }
-    return years;
-}
 
 function updatePoints(points: number, id: string) {
     const validPoints = (points < 0) ? 0 : (points > 100) ? 100 : points;
@@ -119,12 +66,12 @@ function updatePoints(points: number, id: string) {
 }
 
 function doSearch(rename: any, disable: any, inputs: HTMLInputElement[]) {
-    params = {};
-    inputs.forEach(input => params[input.id] = input.value.trim()
+    person = {};
+    inputs.forEach(input => person[input.id] = input.value.trim()
         .toLowerCase().replace(/(([- ]|^)[^ ])/g, (s: string) => s.toUpperCase()));
-    Person.makeName();
+    makeName();
     rename("Загрузка...");
-    Promise.all(searchOlymps()).then(checkTable)
+    Promise.all(searchOlymps(person.pid)).then(checkTable)
         .then(() => {
             rename("Проверить");
             disable(true);
@@ -145,10 +92,10 @@ function clearData() {
 function checkTable(values: any[]) {
     const trs = values.flat();
     if (trs.length) {
-        InsertTable(Person.getDName(), trs);
+        InsertTable(person.Dname, trs);
     } else {
-        if (params.LN) {
-            alert(`Олимпиад РСОШ абитуриента\n${Person.getDName()} не найдено!`);
+        if (person.LN) {
+            alert(`Олимпиад РСОШ абитуриента\n${person.Dname} не найдено!`);
             if (fromWLS) {
                 window.close();
             }
@@ -158,7 +105,7 @@ function checkTable(values: any[]) {
 
 function loadFromWLS() {
     loadParams();
-    Promise.all(searchOlymps()).then(checkTable);
+    Promise.all(searchOlymps(person.pid)).then(checkTable);
 }
 
 if (fromWLS) {
---
+++ src/ts/search.ts
@@ -0,0 +1,47 @@
+import { RSROLYMP } from "./constants";
+
+function getSubTitles(olympName: string) {
+    return {
+        name: olympName.substring(olympName.indexOf('. "') + 3, olympName.indexOf('("') - 2).replace(/[«»]+/g, '"').trim(),
+        lvl: Number(olympName.substr(olympName.indexOf('уровень') - 2, 1).trim()),
+        dip: Number(olympName.substr(olympName.indexOf('Диплом') + 7, 1).trim()),
+        subj: olympName.substring(olympName.indexOf('("') + 2, olympName.indexOf('")')).toLowerCase().replace('cистемы', 'системы').trim(),
+    };
+}
+
+function getOlymps(year: number, codes: any[]) {
+    const trs = [];
+    for (const d of codes) {
+        if (d.form > 9) {
+            trs.push({
+                year: year,
+                code: d.code,
+                grad: d.form,
+                ...getSubTitles(d.oa),
+            });
+        }
+    }
+    return trs;
+}
+
+function loadDiplomaList(year: number, pid: string) {
+    const s = document.createElement('script');
+    s.async = false;
+    s.src = `${RSROLYMP}${year}/by-person-released/${pid}/codes.js`;
+    document.head.appendChild(s);
+    return new Promise(resolve => {
+        s.onload = () => resolve(getOlymps(year, window.diplomaCodes));
+        s.onerror = () => resolve([]);
+    });
+}
+
+function searchOlymps(personID: string) {
+    const currYear = new Date().getFullYear();
+    const years = [];
+    for (let year = currYear; year >= currYear - 4; year--) {
+        years.push(loadDiplomaList(year, personID));
+    }
+    return years;
+}
+
+export { searchOlymps };
